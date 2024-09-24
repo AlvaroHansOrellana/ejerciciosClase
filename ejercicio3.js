9b1aad3d41cbf2ef866dab243d17a4d8fb88275b
A. Hans Orellana <alvarohansorellana@gmail.com>
2024-09-24 16:47:13 +0200

    cambios

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,1 +1,16 @@
-//Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
+// Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
+
+/*const cuentaNombres = (cadena) => cadena.split(" ").length;
+
+console.log(cuentaNombres("Alvaro Hans Orellana Dalsgaard-Bohsen")); */
+
+function cuentaCaracteres(nombre) {
+    return nombre.count("");
+}
+console.log(cuentaCaracteres("Juan"));
+
+
+
+/*const cuentaLetras = (cadena) => cadena.count("").lenght;
+
+console.log(cuentaLetras("jose Luis");*/
--- ejercicio4.js
+++ ejercicio4.js
@@ -1,1 +1,5 @@
 // Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
+
+function(count, num) {
+    let 
+}
