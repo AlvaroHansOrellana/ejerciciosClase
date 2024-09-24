91faaf90f5b844c29fb693198dd1de5ae4806ca6
A. Hans Orellana <alvarohansorellana@gmail.com>
2024-09-24 18:40:30 +0200

    fatal hijo

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,5 +1,10 @@
 // Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
 
-function(count, num) {
-    let 
-}
+
+function imprimirImpares(numero) {
+    for (let i = numero; i <= numero + 50; i++) {
+      if (i % 2 !== 0) console.log(i); 
+    }
+  }
+  console.log(imprimirImpares(15));
+  
